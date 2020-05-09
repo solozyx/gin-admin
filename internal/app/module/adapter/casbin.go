ee05783f99dd8a656f4f277eb57e413f29e7eb1d
solozyx <zhaoyingxin@aliyun.com>
2020-05-09 16:49:05 +0800

    casbin model log debug

--- internal/app/module/adapter/casbin.go
+++ internal/app/module/adapter/casbin.go
@@ -31,12 +31,12 @@ type CasbinAdapter struct {
 // LoadPolicy loads all policy rules from the storage.
 func (a *CasbinAdapter) LoadPolicy(model casbinModel.Model) error {
 
-	logrus.Debugf("CasbinAdapter LoadPolicy 1 model=%+v", model)
-	logrus.Debugf("CasbinAdapter LoadPolicy 1 model[e][e]=%+v", model["e"]["e"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 1 model[g][g]=%+v", model["g"]["g"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 1 model[m][m]=%+v", model["m"]["m"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 1 model[p][p]=%+v", model["p"]["p"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 1 model[r][r]=%+v", model["r"]["r"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 1 model=%+v", model)
+	//logrus.Debugf("CasbinAdapter LoadPolicy 1 model[e][e]=%+v", model["e"]["e"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 1 model[g][g]=%+v", model["g"]["g"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 1 model[m][m]=%+v", model["m"]["m"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 1 model[p][p]=%+v", model["p"]["p"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 1 model[r][r]=%+v", model["r"]["r"])
 
 	ctx := context.Background()
 	err := a.loadRolePolicy(ctx, model)
@@ -51,18 +51,26 @@ func (a *CasbinAdapter) LoadPolicy(model casbinModel.Model) error {
 		return err
 	}
 
-	logrus.Debugf("CasbinAdapter LoadPolicy 2 model=%+v", model)
-	logrus.Debugf("CasbinAdapter LoadPolicy 2 model[e][e]=%+v", model["e"]["e"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 2 model[g][g]=%+v", model["g"]["g"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 2 model[m][m]=%+v", model["m"]["m"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 2 model[p][p]=%+v", model["p"]["p"])
-	logrus.Debugf("CasbinAdapter LoadPolicy 2 model[r][r]=%+v", model["r"]["r"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 2 model=%+v", model)
+	//logrus.Debugf("CasbinAdapter LoadPolicy 2 model[e][e]=%+v", model["e"]["e"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 2 model[g][g]=%+v", model["g"]["g"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 2 model[m][m]=%+v", model["m"]["m"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 2 model[p][p]=%+v", model["p"]["p"])
+	//logrus.Debugf("CasbinAdapter LoadPolicy 2 model[r][r]=%+v", model["r"]["r"])
 
 	return nil
 }
 
 // 加载角色策略(p,role_id,path,method)
 func (a *CasbinAdapter) loadRolePolicy(ctx context.Context, m casbinModel.Model) error {
+
+	logrus.Debugf("CasbinAdapter loadRolePolicy casbinModel.Model m=%+v", m)
+	logrus.Debugf("CasbinAdapter loadRolePolicy casbinModel.Model m[e][e]=%+v", m["e"]["e"])
+	logrus.Debugf("CasbinAdapter loadRolePolicy casbinModel.Model m[g][g]=%+v", m["e"]["e"])
+	logrus.Debugf("CasbinAdapter loadRolePolicy casbinModel.Model m[m][m]=%+v", m["m"]["m"])
+	logrus.Debugf("CasbinAdapter loadRolePolicy casbinModel.Model m[p][p]=%+v", m["p"]["p"])
+	logrus.Debugf("CasbinAdapter loadRolePolicy casbinModel.Model m[r][r]=%+v", m["r"]["r"])
+
 	roleResult, err := a.RoleModel.Query(ctx, schema.RoleQueryParam{
 		Status: 1,
 	})
@@ -72,22 +80,60 @@ func (a *CasbinAdapter) loadRolePolicy(ctx context.Context, m casbinModel.Model)
 		return nil
 	}
 
+	// logrus.Debugf("CasbinAdapter loadRolePolicy roleResult=%+v",roleResult)
+	// logrus.Debugf("CasbinAdapter loadRolePolicy roleResult.Data=%v",roleResult.Data)
+	for sK, sV := range roleResult.Data {
+		logrus.Debugf("CasbinAdapter loadRolePolicy roleResult.Data sK=%+v sV=%+v", sK, sV)
+	}
+
 	roleMenuResult, err := a.RoleMenuModel.Query(ctx, schema.RoleMenuQueryParam{})
 	if err != nil {
 		return err
 	}
+
+	// logrus.Debugf("CasbinAdapter loadRolePolicy roleMenuResult=%+v",roleMenuResult)
+	// logrus.Debugf("CasbinAdapter loadRolePolicy roleMenuResult.Data=%+v",roleMenuResult.Data)
+	for sK, sV := range roleMenuResult.Data {
+		logrus.Debugf("CasbinAdapter loadRolePolicy roleMenuResult.Data sK=%+v sV=%+v", sK, sV)
+	}
+
 	mRoleMenus := roleMenuResult.Data.ToRoleIDMap()
+
+	// logrus.Debugf("CasbinAdapter loadRolePolicy mRoleMenus=%+v",mRoleMenus)
+	for mK, mV := range mRoleMenus {
+		logrus.Debugf("CasbinAdapter loadRolePolicy mRoleMenus mK=%+v", mK)
+		for sK, sV := range mV {
+			logrus.Debugf("CasbinAdapter loadRolePolicy mRoleMenus mK=%+v sK=%+v sV=%+v", mK, sK, sV)
+		}
+	}
 
 	menuResourceResult, err := a.MenuResourceModel.Query(ctx, schema.MenuActionResourceQueryParam{})
 	if err != nil {
 		return err
 	}
+
+	// logrus.Debugf("CasbinAdapter loadRolePolicy menuResourceResult=%+v",menuResourceResult)
+	// logrus.Debugf("CasbinAdapter loadRolePolicy menuResourceResult.Data=%+v",menuResourceResult.Data)
+	for sK, sV := range menuResourceResult.Data {
+		logrus.Debugf("CasbinAdapter loadRolePolicy menuResourceResult.Data sK=%+v sV=%+v", sK, sV)
+	}
+
 	mMenuResources := menuResourceResult.Data.ToActionIDMap()
+
+	// logrus.Debugf("CasbinAdapter loadRolePolicy mMenuResources=%+v",mMenuResources)
+	for mK, mV := range mMenuResources {
+		logrus.Debugf("CasbinAdapter loadRolePolicy mMenuResources mK=%+v", mK)
+		for sK, sV := range mV {
+			logrus.Debugf("CasbinAdapter loadRolePolicy mMenuResources mK=%+v sK=%+v sV=%+v", mK, sK, sV)
+		}
+	}
 
 	mcache := make(map[string]struct{})
 	for _, item := range roleResult.Data {
-		if rms, ok := mRoleMenus[item.RecordID]; ok {
-			for _, actionID := range rms.ToActionIDs() {
+		if rms, ok := mRoleMenus[item.RecordID]; ok { // item.RecordID 5eaaa01912a12014ac2b6ef1 角色id rms长度为7
+			logrus.Debugf("rms.ToActionIDs()=%+v", rms.ToActionIDs())
+			// "rms.ToActionIDs()=[ 5eaa744cec65f5408adfa49c 5eaa744cec65f5408adfa49a 5eaa744cec65f5408adfa498 5eaa744cec65f5408adfa496 5eaa744cec65f5408adfa493 5eaa744cec65f5408adfa491]"
+			for _, actionID := range rms.ToActionIDs() { // action_id
 				if mrs, ok := mMenuResources[actionID]; ok {
 					for _, mr := range mrs {
 						if mr.Path == "" || mr.Method == "" {
