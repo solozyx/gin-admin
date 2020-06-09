a7f8e3b61952c2bfe0164bec5b8f92d7eba501f8
solozyx <zhaoyingxin@aliyun.com>
2020-06-09 14:08:41 +0800

    btree

--- _demo/fen_yuan.go
+++ _demo/fen_yuan.go
@@ -6,14 +6,24 @@ import (
 )
 
 func main() {
-	fen2yuan(10001)
+	fmt.Println(fen2yuan(1))
+	fmt.Println(fen2yuan(10))
+	fmt.Println(fen2yuan(100))
+
+	fmt.Println(fen2yuan(11))
+	fmt.Println(fen2yuan(101))
+	fmt.Println(fen2yuan(1001))
 }
 
 func fen2yuan(fen int) string {
 	fenStr := strconv.Itoa(fen)
+	if len(fenStr) == 1 {
+		return fmt.Sprintf("0.0%s", fenStr)
+	} else if len(fenStr) == 2 {
+		return fmt.Sprintf("0.%s", fenStr)
+	}
 	firstStr := fenStr[:len(fenStr)-2]
 	lastStr := fenStr[len(fenStr)-2:]
 	yuan := fmt.Sprintf("%s.%s", firstStr, lastStr)
-	fmt.Printf("fen2yuan yuan=%s", yuan)
 	return yuan
 }
--- docs/secondchase/secondchase.sql
+++ docs/secondchase/secondchase.sql
@@ -143,6 +143,7 @@ CREATE TABLE `mz_product` (
   `ship_address` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '发货地址',
   `sale_support` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '售后保障',
   `description` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '描述备注',
+  `reward` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '购买单个商品增加的抽奖次数',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
@@ -159,10 +160,47 @@ update mz_product set `name`='小米 Redmi Note8 小金刚品质保证 高通骁
  `banner_urls`='https://secondschased.oss-cn-beijing.aliyuncs.com/lottery/xiaomi.png,https://secondschased.oss-cn-beijing.aliyuncs.com/lottery/lihe.png',
  `sku`='小金刚, 4GB+64GB',
  `type`=1,`product_status`=1,
- `ship_address`='北京',`sale_support`='官方授权 秒追直发 假一赔四 品质保证'
+ `ship_address`='北京',`sale_support`='官方授权 秒追直发 假一赔四 品质保证',
+ `reward`=1
  where id=1;
+
+update mz_product set `reward`=5 where id=1;
 
 -- 运费要根据地址算
 
+show create table mz_product;
 alter table mz_product drop column `sku_desc`;
 ALTER TABLE `mz_product` ADD `sku` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'sku描述' AFTER `banner_urls`;
+ALTER TABLE `mz_product` DROP `lottery_times`;
+ALTER TABLE `mz_product` ADD `reward` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '购买单个商品增加的抽奖次数' AFTER `description`;
+
+
+CREATE TABLE `mz_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '任务ID',
+  `task_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '任务名称',
+  `reward` int(11) NOT NULL COMMENT '任务奖励',
+  `type` int(11) NOT NULL COMMENT '类型',
+  `status` int(11) NOT NULL COMMENT '任务状态',
+  `flags` int(11) NOT NULL DEFAULT '0' COMMENT '标识位 ',
+  `details` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '详情',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `pid` int(11) NOT NULL DEFAULT '0' COMMENT '商品ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务列表';
+
+create index index_pid on `mz_task`(`pid`) using btree;
+
+CREATE TABLE `mz_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '任务ID',
+  `task_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '任务名称',
+  `reward` int(11) NOT NULL COMMENT '任务奖励',
+  `type` int(11) NOT NULL COMMENT '类型',
+  `status` int(11) NOT NULL COMMENT '任务状态',
+  `flags` int(11) NOT NULL DEFAULT '0' COMMENT '标识位 ',
+  `details` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '详情',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `pid` int(11) NOT NULL DEFAULT '0' COMMENT '商品ID',
+  PRIMARY KEY (`id`),
+  KEY `index_pid` (`pid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='任务列表';
+
